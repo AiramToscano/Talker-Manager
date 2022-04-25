c9122add2eb4ad0fffc4b46d9d3951e279979e82
Airam Toscano <airamlobato@gmail.com>
2022-04-25 16:32:50 -0400

    exercicio 3 finalizado

--- index.js
+++ index.js
@@ -37,3 +37,15 @@ app.get('/talker/:id', async (req, res) => {
   if (filterId) return res.status(HTTP_OK_STATUS).json(filterId);
 return res.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
 });
+
+function geraStringAleatoria(_req, res, _next) {
+  let stringAleatoria = '';
+  // https://www.webtutorial.com.br/funcao-para-gerar-uma-string-aleatoria-random-com-caracteres-especificos-em-javascript/#:~:text=A%20fun%C3%A7%C3%A3o%20Math.,baixo%20(com%20a%20fun%C3%A7%C3%A3o%20Math.
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 16; i += 1) {
+      stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return res.status(200).json({ token: stringAleatoria });
+}
+
+app.post('/login', geraStringAleatoria);
